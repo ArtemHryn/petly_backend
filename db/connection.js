baefcf50e2ae86dd2f5ab6957a486f9f0db0439e
ArtemHryn <grynbest@gmail.com>
2023-02-13 20:43:02 +0200

    DB connection for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
+const DB_CONNECTION = process.env.MONGO_CONNECTION_DB
+  ? process.env.MONGO_CONNECTION_DB
+  : `mongodb+srv://production:generatorpass@pets.iomwoyz.mongodb.net/pets`;
+
 const connectMongo = async () => {
   mongoose.set("strictQuery", false);
-  mongoose.connect(process.env.MONGO_CONNECTION_DB, {
+  mongoose.connect(DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
