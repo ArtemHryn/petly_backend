b7d8b532a1318a295cdbfe16126820812ff5d1e1
Alina Proskurnitska <proskur1987@gmail.com>
2023-02-25 20:59:11 +0200

    added timestamps

--- models/newsModel.js
+++ models/newsModel.js
@@ -1,23 +1,26 @@
-const { Schema, model } = require("mongoose");
-const { ErrorConstructor } = require("../helper/errors");
+const { Schema, model } = require('mongoose');
+const { ErrorConstructor } = require('../helper/errors');
 
-const newsSchema = new Schema({
-  title: {
-    type: String,
+const newsSchema = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    date: {
+      type: String,
+    },
   },
-  url: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  date: {
-    type: String,
-  },
-});
+  { versionKey: false, timestamps: { createdAt: true, updatedAt: false } }
+);
 
-newsSchema.post("save", ErrorConstructor);
+newsSchema.post('save', ErrorConstructor);
 
-const News = model("new", newsSchema);
+const News = model('new', newsSchema);
 
 module.exports = { News };
