8da4dfb85cdfb1cb722092302aaa9bc17b64089c
ArtemHryn <grynbest@gmail.com>
2023-02-27 03:35:48 +0200

    fix

--- helper/checkCredentials.js
+++ helper/checkCredentials.js
@@ -5,7 +5,11 @@ const checkCredentials = async (user, password) => {
   if (!user) {
     throw new ErrorConstructor(401, 'Email or password is wrong');
   }
-  if (!(await bcrypt.compare(password, user.password))) {
+  const hashPass = bcrypt.compareSync(password, user.password);
+  // if (!(await bcrypt.compare(password, user.password))) {
+  //   throw new ErrorConstructor(401, 'Email or password is wrong');
+  // }
+  if (!hashPass) {
     throw new ErrorConstructor(401, 'Email or password is wrong');
   }
 };
