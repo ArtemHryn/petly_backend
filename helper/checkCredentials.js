fc72301f058340d14fee4189d1637d7bc0d7f729
ArtemHryn <grynbest@gmail.com>
2023-02-27 03:37:59 +0200

    hash pass

--- helper/checkCredentials.js
+++ helper/checkCredentials.js
@@ -5,11 +5,11 @@ const checkCredentials = async (user, password) => {
   if (!user) {
     throw new ErrorConstructor(401, 'Email or password is wrong');
   }
-  const hashPass = bcrypt.compareSync(password, user.password);
+  const hashPass = await bcrypt.hash(password, 10)
   // if (!(await bcrypt.compare(password, user.password))) {
   //   throw new ErrorConstructor(401, 'Email or password is wrong');
   // }
-  if (!hashPass) {
+  if (hashPass !== user.password) {
     throw new ErrorConstructor(401, 'Email or password is wrong');
   }
 };
